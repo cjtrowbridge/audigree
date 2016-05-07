014d85dc038c2547ef61d3733846731f0a65039b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-05-07 14:05:18 -0700

    Create audigree.php
    Added comment block to main file

---
+++ audigree.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Plugin Name: Audigree
+* Description: Create formatted pedigrees automatically
+* Version: 1.0
+* Author: CJ Trowbridge
+* Author URI: https://github.com/audigree
+*/
+
+?>
