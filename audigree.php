fd82ebd5f4c8cd20336c470f09766dd4c801727b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-05-07 14:07:01 -0700

    Update audigree.php
    Added shortcode function and sample layout

--- audigree.php
+++ audigree.php
@@ -7,4 +7,16 @@
 * Author URI: https://github.com/audigree
 */
 
+function audigree_automatic_pedigree(){
+  ?>
+  <b>Siblings:</b>
+  <ul>
+    <li>sibling a</li>
+    <li>sibling b</li>
+  </ul>
+  <?php
+}
+
+add_shortcode('audigree_automatic_pedigree', 'audigree_automatic_pedigree');
+
 ?>
