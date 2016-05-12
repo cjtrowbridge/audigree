3d0ca158ee2922fc4822d7389e4f4e138f254b05
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-05-12 10:57:10 -0700

    Update audigree.php

--- audigree.php
+++ audigree.php
@@ -8,26 +8,35 @@
 */
 
 function audigree_automatic_pedigree(){
-  ?>
   
-  <p><b>Current Page:</b> <?php var_dump(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)); ?></p>
+  $path=parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
   
-  <p><b>Father: </b> <a href="#">Father's Name</a></p>
-  <p><b>Mother: </b> <a href="#">Mother's Name</a></p>
+  if(substr_count($path, '/')>=2){
+    //this is a single argument path, and probably a page for a person
+    $page_name=str_replace('/','',$path);
+    $page_name=strtolower($page_name);
+    ?>
   
-  <b>Siblings:</b>
-  <ul>
-    <li>sibling a</li>
-    <li>sibling b</li>
-  </ul>
+      <p><b>Current Page:</b> <?php $page_name; ?></p>
+      
+      <p><b>Father: </b> <a href="#">Father's Name</a></p>
+      <p><b>Mother: </b> <a href="#">Mother's Name</a></p>
+      
+      <b>Siblings:</b>
+      <ul>
+        <li>sibling a</li>
+        <li>sibling b</li>
+      </ul>
+      
+      <b>Children:</b>
+      <ul>
+        <li>child a</li>
+        <li>child b</li>
+      </ul>
+    <?php
+  }
   
-  <b>Children:</b>
-  <ul>
-    <li>child a</li>
-    <li>child b</li>
-  </ul>
   
-  <?php
 }
 
 add_shortcode('audigree_automatic_pedigree', 'audigree_automatic_pedigree');
