dcaf25be089942f12333dbb894f53e9351332be1
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-05-12 12:29:50 -0700

    Update audigree.php

--- audigree.php
+++ audigree.php
@@ -6,33 +6,7 @@
 * Author: CJ Trowbridge
 * Author URI: https://github.com/audigree
 */
-function Audigree_Get_Siblings_By_Parent_IDs($id1,$id2,$self_id){
-  global $wpdb; /*this is the object that lets us run queries*/
-  
-  $id1=intval($id1);
-  $id2=intval($id2);
-  if(
-    ($id1==0)||
-    ($id2==0)
-  ){
-    echo 'Person Not Found';
-    return;
-  }
-    $sql="
-      SELECT person_id
-      FROM audigree_person 
-      WHERE 
-      person_id NOT LIKE ".$self_id." AND
-      (
-        mother_id = '".$id1."' OR
-        father_id = '".$id1."' OR
-        mother_id = '".$id2."' OR
-        father_id = '".$id2."'
-      )
-    ";
-    $siblings = $wpdb->get_results($sql, OBJECT);
-    return $siblings;
-}
+
 function Audigree_Get_Children_By_Parent_ID($id){
   global $wpdb; /*this is the object that lets us run queries*/
   
