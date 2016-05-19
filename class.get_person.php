8e75ed211fa17c2ce4f873983f7d0223cccd6cbd
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-05-19 10:08:19 -0700

    Update class.get_person.php
    removed maiden name

--- class.get_person.php
+++ class.get_person.php
@@ -43,8 +43,7 @@ function Audigree_Get_Person($query){
       $sql.="
         AND 
         (
-          name_last LIKE '".$wpdb->esc_like($this_person_name_parts[2])."' OR
-          name_maiden LIKE '".$wpdb->esc_like($this_person_name_parts[2])."'
+          name_last LIKE '".$wpdb->esc_like($this_person_name_parts[2])."'
         )
       ";
     }
